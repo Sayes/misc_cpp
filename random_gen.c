941db7afb2a7bc0bc357a7ee22607ba9a839eb26
shenyizhong <shenyizhong@seetatech.com>
2019-09-02 21:35:20 +0800

    * code review

--- random_gen.c
+++ random_gen.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<sys/time.h>
+#include <sys/time.h>
 
 int main ()
 {
+  int RANGE_MIN = 1000;
+  int RANGE_MAX = 3000;
   for (int i = 0; i < 100; ++i) {
     struct timeval tpstart;
     gettimeofday(&tpstart, NULL);
     srand(tpstart.tv_usec);
     int random = rand();
-    printf("%d\n", random);
+    printf("%d\n", (int)((random*1.0f/RAND_MAX)*(RANGE_MAX-RANGE_MIN) + RANGE_MIN));
     random = rand();
-    printf("%d\n", random);
+    printf("%d\n", (int)((random*1.0f/RAND_MAX)*(RANGE_MAX-RANGE_MIN) + RANGE_MIN));
     random = rand();
-    printf("%d\n", random);
+    printf("%d\n", (int)((random*1.0f/RAND_MAX)*(RANGE_MAX-RANGE_MIN) + RANGE_MIN));
   }
 }
